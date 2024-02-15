47e17dbbbf8e205dd461c8e11695c23fb4a142ab
1JuL <diegodiax0122@gmail.com>
2024-02-15 00:01:59 -0500

    Tailwind webpage v1

---
+++ tailwindcss-workshop/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [],
+  theme: {
+    colors: {
+      'btngray': '#303B5A',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
